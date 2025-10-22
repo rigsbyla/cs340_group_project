df2f9dfb2f335b21c5067f04dd289072d2a86cb6
Mael <126711069+coherenspto23@users.noreply.github.com>
2025-10-21 18:20:37 -0700

    Update MaelDDL.sql

--- MaelDDL.sql
+++ MaelDDL.sql
@@ -6,6 +6,13 @@ CREATE TABLE Books(
   quantity INT NOT NULL,
   PRIMARY KEY (book_id)
   ) ENGINE = InnoDB;
+CREATE TABLE Genres (
+  genre_id INT AUTO_INCREMENT NOT NULL,
+  genre_name VARCHAR(45) NOT NULL,
+  PRIMARY KEY (genre_id)
+  ) ENGINE = InnoDB;
+
+
 INSERT INTO Books(book_id, title, call_num, qunatity)
 VALUES 
   (1, 'The Rangers of the North: A New beginning', 'CF4362', 4),
@@ -16,3 +23,13 @@ VALUES
   (6, 'Car Owners Guide for Toyota Corolla 2004', 'EG0082', 1),
   (7, 'Atlas of the World', 'GE4012', 2),
   (8, 'Dinosaurs!', 'SC552', 2);
+
+INSERT INTO Genres(genre_id, genre_name)
+VALUES
+  (1, 'Fiction'),
+  (2, 'Non Fiction'),
+  (3, 'History'),
+  (4, 'Reference'),
+  (5, 'Textbook'),
+  (6, 'Science Fiction'),
+  (7, 'Music');
