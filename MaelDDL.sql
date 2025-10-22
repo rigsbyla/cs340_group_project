d3ee16949103e302193e9d3e40c4298e52bc1c83
Mael <126711069+coherenspto23@users.noreply.github.com>
2025-10-21 18:08:59 -0700

    Enhance Books table structure with additional fields

--- MaelDDL.sql
+++ MaelDDL.sql
@@ -1,3 +1,8 @@
 -- Test Comment
 CREATE TABLE Books( 
-  book_id INT);
+  book_id INT NOT NULL AUTO_INCREMENT UNIQUE,
+  title VARCHAR(145) NOT NULL,
+  call_num CHAR(6),
+  quantity INT NOT NULL,
+  PRIMARY KEY (book_id)
+  ) ENGINE = InnoDB;
