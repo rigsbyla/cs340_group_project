f590bf3a5009ace83228abb2d1f46ba3dc956dbc
rigsbyla <rigsbyla@oregonstate.edu>
2025-10-31 14:19:35 -0700

    Made Book order pages

--- project/app.js
+++ project/app.js
@@ -82,6 +82,67 @@ app.get('/members', async function (req, res) {
     }
 });
 
+
+app.get('/orders', async function (req, res) {
+    try {
+        // Create and execute our queries
+        const query1 = `
+        SELECT Orders.order_id, 
+        Members.first_name AS member, 
+        Members.last_name AS member_last_name, 
+        Orders.order_date, 
+        Orders.due_date FROM Orders
+
+            INNER JOIN Members ON Orders.Member_id =  Members.member_id ;`;
+        const [orders] = await db.query(query1);
+
+        // Format Dates
+        orders.forEach(o => {
+            o.order_date = o.order_date.toISOString().split('T')[0];
+            o.due_date = o.due_date.toISOString().split('T')[0];
+        });
+
+        res.render('orders', { orders: orders });
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.get('/orders-new', async function (req, res) {
+    try {
+        res.render('orders-new'); // Render the home.hbs file
+    } catch (error) {
+        console.error('Error rendering page:', error);
+        // Send a generic error message to the browser
+        res.status(500).send('An error occurred while rendering the page.');
+    }
+});
+
+app.get('/order-details', async function (req, res) {
+    try {
+        const query1 = `
+        SELECT BookOrders.book_order_id, 
+        BookOrders.order_id, 
+        Books.title AS book_title, 
+        BookOrders.quantity, 
+        Orders.member_id AS member_id
+        FROM BookOrders
+        INNER JOIN Books ON BookOrders.book_id = Books.book_id
+        INNER JOIN Orders ON BookOrders.order_id = Orders.member_id;`;
+        const [orderdetail] = await db.query(query1);
+        res.render('order-details', { orderdetail: orderdetail }); // Render the home.hbs file
+    } catch (error) {
+        console.error('Error rendering page:', error);
+        // Send a generic error message to the browser
+        res.status(500).send('An error occurred while rendering the page.');
+    }
+});
+
 // ########################################
 // ########## LISTENER
 
