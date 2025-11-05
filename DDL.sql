35ab42f01d2b37a8c3f1fb5f9c6c361383ace975
Mael <126711069+coherenspto23@users.noreply.github.com>
2025-11-04 17:41:22 -0800

    Fix typo in 'Settlements' in DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -85,7 +85,7 @@ VALUES
   (1, 'The Rangers of the North: A New beginning', 'CF4362', 4),
   (2, 'The Rangers of the North: A Kingdom in Peril', 'CF4367', 5),
   (3, 'Plants of the Pacific North West', 'SC7839', 2),
-  (4, 'Early Settelments of the Willamette Valley', 'HS3312', 2),
+  (4, 'Early Settlments of the Willamette Valley', 'HS3312', 2),
   (5, 'Introduction to Calculus', 'MT5721', 6),
   (6, 'Car Owners Guide for Toyota Corolla 2004', 'EG0082', 1),
   (7, 'Atlas of the World', 'GE4012', 2),
